d8c20293d66e06ec2caa9313453d8b038b83f9fe
DevilsGambit22 <isaiasflash@hotmail.com>
2026-07-15 04:18:39 -0500

    Update config.js

--- shared/config.js
+++ shared/config.js
@@ -1,4 +1,4 @@
 window.ACFA_CONFIG = {
-  supabaseUrl: "https://ie7yhqzhrdkrrfsdqhmch.supabase.co",
+  supabaseUrl:"https://leryhqzhrdkrfsdqbmch.supabase.co",
   supabasePublishableKey:  "sb_publishable_TSAqo3OxVgP97n2b7yeQCw_zuBWltp8";
 };
